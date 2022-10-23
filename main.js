9dc2bce8a5440710fabcdd4f89a8cd61fb9e8c31
Nicergrossa123 <94936693+Nicergrossa123@users.noreply.github.com>
2022-10-23 23:22:32 +0200

    first commit

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const log = require('electron-log');
 log.transports.file.resolvePath = () => path.join('C:\Users\Samuele#\Desktop\UAUP-Example-main', 'logs/main.log');
 log.info('Hello');
 log.warn('xxx')
+log.log('app version' +app.getVersion())
 let win;
 function  createWindow(){
 win = new BrowserWindow({width:300,height:400})
@@ -15,17 +16,28 @@ app.on('ready',() =>{
     createWindow()
     autoUpdater.checkForUpdatesAndNotify()
 })
-
-autoUpdater.on("update-aviable", () => {
+autoUpdater.on("update-not-aviable", (info) => {
+    log.info("update-aviable")
+})
+autoUpdater.on("update-aviable", (info) => {
     log.info("update-aviable")
 })
 
 autoUpdater.on("check-for-update", () => {
     log.info("check-for-update")
 })
+autoUpdater.on("error", (err) => {
+    log.info("error" + err)
+})
+
 autoUpdater.on("download", () => {
     log.info("download")
 })
-autoUpdater.on("fertig", () => {
+
+autoUpdater.on("download-p", (progressTrack) =>{
+    log.info("\n\ndownload-p")
+    log.info(pregressTrack)
+})
+autoUpdater.on("fertig", (info) => {
     log.info("fertig")
 })
